f655878d928244a270e75505c24a1e036883947b
Mike Hepburn <eformat@gmail.com>
2025-07-03 10:12:16 +1000

    🦊 vault setup script 🦊

--- gitops/bootstrap/vault-setup.sh
+++ gitops/bootstrap/vault-setup.sh
@@ -9,6 +9,10 @@ readonly NC='\033[0m' # No Color
 # setup secrets for gitops
 # https://eformat.github.io/rainforest-docs/#/2-platform-work/3-secrets
 
+[ -z "$AWS_PROFILE" ] && echo "🕱 Error: must supply AWS_PROFILE in env" && exit 1
+[ -z "$BASE_DOMAIN" ] && echo "🕱 Error: must supply BASE_DOMAIN in env" && exit 1
+[ -z "$ADMIN_PASSWORD" ] && echo "🕱 Error: must supply ADMIN_PASSWORD in env" && exit 1
+
 # use login
 export KUBECONFIG=~/.kube/config.${AWS_PROFILE}
 
@@ -106,7 +110,7 @@ fi
 init () {
     echo "💥 Init Vault..."
     local i=0
-    oc -n vault exec vault-0 -- vault operator init -key-threshold=1 -key-shares=1 -tls-skip-verify 2>&1 | tee /tmp/vault-init
+    oc -n vault exec vault-0 -- vault operator init -key-threshold=1 -key-shares=1 -tls-skip-verify 2>&1 | tee /tmp/vault-init-${AWS_PROFILE}
     until [ "$?" == 0 ]
     do
         echo -e "${GREEN}Waiting for 0 rc from oc commands.${NC}"
@@ -116,14 +120,14 @@ init () {
             exit 1
         fi
         sleep 10
-        oc -n vault exec vault-0 -- vault operator init -key-threshold=1 -key-shares=1 -tls-skip-verify 2>&1 | tee /tmp/vault-init
+        oc -n vault exec vault-0 -- vault operator init -key-threshold=1 -key-shares=1 -tls-skip-verify 2>&1 | tee /tmp/vault-init-${AWS_PROFILE}
     done
     echo "💥 Init Vault Done"
 }
 init
 
-export UNSEAL_KEY=$(cat /tmp/vault-init | grep -e 'Unseal Key 1' | awk '{print $4}')
-export ROOT_TOKEN=$(cat /tmp/vault-init | grep -e 'Initial Root Token' | awk '{print $4}')
+export UNSEAL_KEY=$(cat /tmp/vault-init-${AWS_PROFILE} | grep -e 'Unseal Key 1' | awk '{print $4}')
+export ROOT_TOKEN=$(cat /tmp/vault-init-${AWS_PROFILE} | grep -e 'Initial Root Token' | awk '{print $4}')
 
 oc -n vault exec vault-0 -- vault operator unseal -tls-skip-verify $UNSEAL_KEY
 if [ "$?" != 0 ]; then
